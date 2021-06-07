4aa201982295ec77f8f82a80351676eacc8ef8da
Ben <benmali94@gmail.com>
2021-06-07 15:51:59 +0300

    test process

--- graphs/AUC.py
+++ graphs/AUC.py
@@ -9,20 +9,6 @@ class AUC:
         self.fig.suptitle('K Folds')
         self.index_map = {0: [0, 0], 1:[0,1],2:[1,0],3:[1,1]}
 
-    def plot_roc_curve(self, fpr, tpr, auc):
-        plt.title('Receiver Operating Characteristic')
-        blue_patch = mpatches.Patch(color='blue', label='Mean AUC test = %0.2f' % auc)
-        gray_patch = mpatches.Patch(color='gray', label='K-folds')
-        red_patch = mpatches.Patch(color='red', label='Random Classifier', ls='--')
-        plt.legend(handles=[blue_patch, gray_patch, red_patch], loc='lower right')
-        plt.plot([0, 1], [0, 1], 'r--')
-        plt.xlim([0, 1])
-        plt.ylim([0, 1])
-        plt.ylabel('True Positive Rate')
-        plt.xlabel('False Positive Rate')
-        plt.plot(fpr, tpr, color='blue')
-        plt.show()
-
     def plot_auc(self, name, kfold_fpr_tpr, fpr, tpr, mean_auc, i):
         blue_patch = mpatches.Patch(color='blue', label='Mean AUC test = %0.2f' % mean_auc)
         gray_patch = mpatches.Patch(color='gray', label='K-folds')
--- processing/test_process.py
+++ processing/test_process.py
@@ -1,5 +1,17 @@
 import pandas as pd
 from processing.basic_process import basic_process
-df = pd.read_csv('train.csv')
-df = basic_process(df)
-final_data = []
+from classifiers.ANN import ann
+from sklearn.decomposition import PCA
+
+
+def predict_test(data, imputer, scaler, pca):
+    data = pd.read_csv('train.csv')
+    feature_names = list(data.columns.values)  # save column names
+    data = basic_process(data)
+    data = pd.DataFrame(imputer.transform(data))  # impute data
+    data = data.set_axis(feature_names, axis=1, inplace=False)  # rename columns after imputing
+    data = pd.DataFrame(pca.transform(data.drop(columns=['label'], axis=1)))
+
+
+
+
